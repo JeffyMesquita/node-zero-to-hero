60b1eada5db59c170bff5377d31414b22e790d9e
Jeferson Mesquita <je_2742@hotmail.com>
2023-09-07 13:31:22 -0300

    first commit

---
+++ NOTES.md
@@ -0,0 +1,29 @@
+# 1. A simple API with Node.js and Fastify
+
+## 1.1 Requirements
+
+- Node.js 12+
+- npm 6+
+- Docker 19+ (optional)
+
+## 1.2 Setup
+
+### 1.2.1 Initialize the project
+
+```bash
+npm init -y
+```
+
+### 1.2.2 Install dependencies
+
+```bash
+npm i fastify
+```
+
+```bash
+npm install postgres
+```
+
+```bash
+npm install dotenv -D
+```
---
+++ create-table.js
@@ -0,0 +1,24 @@
+import { sql } from './db.js';
+
+// sql`DROP TABLE IF EXISTS videos`
+//   .then(() => {
+//     console.log('Table dropped');
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+sql`
+  CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    duration INTEGER
+  )
+`
+  .then(() => {
+    console.log('Table created');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
